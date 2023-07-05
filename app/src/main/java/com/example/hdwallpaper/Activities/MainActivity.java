3e73decb051cd8e0cc9b9c067d2925890e64ca08
mac <mac@macs-mbp-2.home>
2023-07-05 23:48:03 +0500

    New Data Added

--- app/src/main/java/com/example/hdwallpaper/Activities/MainActivity.java
+++ app/src/main/java/com/example/hdwallpaper/Activities/MainActivity.java
@@ -6,19 +6,52 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.example.hdwallpaper.Adapter.NaturalImagesAdapter;
+import com.example.hdwallpaper.Model.NaturalImages;
 import com.example.hdwallpaper.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView rvNaturalImages;
     NaturalImagesAdapter naturalImagesAdapter;
+    private List<NaturalImages> imageList;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rvNaturalImages  = findViewById(R.id.rvNaturalImages);
+        FirebaseApp.initializeApp(MainActivity.this);
 
+        rvNaturalImages  = findViewById(R.id.rvNaturalImages);
+        imageList = new ArrayList<>();
+        naturalImagesAdapter = new NaturalImagesAdapter(imageList, this);
+        rvNaturalImages.setAdapter(naturalImagesAdapter);
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference().child("Nature"); // Replace "images" with your Firebase Storage folder name
+
+        fetchImages();
+    }
+    private void fetchImages() {
+        storageRef.listAll()
+                .addOnSuccessListener(listResult -> {
+                    for (StorageReference item : listResult.getItems()) {
+                        item.getDownloadUrl().addOnSuccessListener(uri -> {
+                            imageList.add(new NaturalImages(uri.toString()));
+                            naturalImagesAdapter.notifyDataSetChanged();
+                        });
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    // Handle any errors
+                });
     }
 }
